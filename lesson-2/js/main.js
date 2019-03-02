d88321ae5449ab037445c63fda9787b308565201
Tatiana <batagaeva.ta@gmail.com>
2019-03-03 01:36:10 +0800

    Добавлен ответ на урок 2

--- lesson-2/js/main.js
+++ lesson-2/js/main.js
@@ -13,14 +13,48 @@ let appData = {
   saving: false
 };
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//   let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//       b = prompt('Во сколько обойдется? (руб.)', '');
+//   if ((typeof(a) === 'string') && (typeof(a) != null) && (typeof(b) != null) && (a != '') && (b != '') && a.length < 50) {
+
+//     appData.expenses[a] = b;
+//   }
+// }
+
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//       b = prompt('Во сколько обойдется? (руб.)', '');
+//   if ((typeof(a) === 'string') && (typeof(a) != null) && (typeof(b) != null) && (a != '') && (b != '') && a.length < 50) {
+
+//     appData.expenses[a] = b;
+//   }
+//   i++;
+// }
+
+let i = 0;
+do {
   let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
       b = prompt('Во сколько обойдется? (руб.)', '');
   if ((typeof(a) === 'string') && (typeof(a) != null) && (typeof(b) != null) && (a != '') && (b != '') && a.length < 50) {
 
     appData.expenses[a] = b;
   }
-}
+  i++;
+} while (i < 2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
 console.log(appData);
-alert(appData.budget / 30);
+
+if (appData.moneyPerDay < 100){
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000){
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay >= 2000){
+  console.log("Высокий уровень достатка");
+} else
+  console.log("Произошла ошибка");

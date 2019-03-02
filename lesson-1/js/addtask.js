37b87d281a5e1c18fbad3b9a903527edbe26543a
Tatiana <batagaeva.ta@gmail.com>
2019-03-03 01:16:19 +0800

    Внесены правкипо доп  дз

--- lesson-1/js/addtask.js
+++ lesson-1/js/addtask.js
@@ -1,7 +1,9 @@
-var num = 33721;
+'use strict';
 
-pow = eval(num.toString().split('').join('*'));
+let num = 33721;
+
+let pow = eval(num.toString().split('').join('*'));
 console.log(pow);
 pow *= pow * pow;
 
-alert(pow.toString().split('')[0] + pow.toString().split('')[1]);
+alert(pow.toString().substr(0, 2));
---
+++ lesson-2/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let money = prompt("Ваш бюджет на месяц? (руб.)", '30000');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '2019-03-01');
+
+
+let appData = {
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: null,
+  timeData: time,
+  saving: false
+};
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+      b = prompt('Во сколько обойдется? (руб.)', '');
+  if ((typeof(a) === 'string') && (typeof(a) != null) && (typeof(b) != null) && (a != '') && (b != '') && a.length < 50) {
+
+    appData.expenses[a] = b;
+  }
+}
+
+console.log(appData);
+alert(appData.budget / 30);

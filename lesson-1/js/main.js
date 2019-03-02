79539634651335501181c96e35bbd3b49025bc3a
Tatiana <batagaeva.ta@gmail.com>
2019-03-03 00:57:07 +0800

    Внесены правкипо основному дз

--- lesson-1/js/main.js
+++ lesson-1/js/main.js
@@ -1,24 +1,26 @@
 'use strict';
 
-  let money = prompt("Ваш бюджет на месяц? (руб.)", '30000');
-  let time = prompt("Введите дату в формате YYYY-MM-DD", '2019-03-01');
+let money = prompt("Ваш бюджет на месяц? (руб.)", '30000');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '2019-03-01');
 
- 
-  let appData = {
-    budget: money,
-    expenses: {},
-    optionalExpenses: {},
-    income: null,
-    timeData: time,
-    saving: false
-  };
 
-  let a1 = prompt('Введите обязательную статью расходов в этом месяце', 'food'),
-      a2 = prompt('Во сколько обойдется? (руб.)', '3000'),
-      a3 = prompt('Введите обязательную статью расходов в этом месяце', 'clouse'),
-      a4 = prompt('Во сколько обойдется? (руб.)', '2000');
+let appData = {
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: null,
+  timeData: time,
+  saving: false
+};
 
-  appData.expenses.a1 = a2;
-  appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+      b = prompt('Во сколько обойдется? (руб.)', '');
+  // if ((typeof(a) === 'string') && (typeof(a) != null)) {
 
-  alert (appData.budget / 30);
+  // }
+  appData.expenses[a] = b;
+}
+
+console.log(appData);
+alert(appData.budget / 30);

6b0ddd2425b4dc6090ba8494f95682aa3ea08c68
Tatiana <batagaeva.ta@gmail.com>
2019-03-04 16:54:46 +0800

    Добавлено дополнительное задание 3 урок

--- lesson-3/js/addtask.js
+++ lesson-3/js/addtask.js
@@ -1,32 +1,55 @@
 'use strict';
 
-let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-let currentDay = 'вс';
+// Задание 1
+let str = 'урок-3-был слишком легким';
 
-for (let i = 0; i < week.length; i++) {
-  switch (week[i]) {
-    case 'сб':
-    case 'вс':
-      if (week[i] == currentDay) {
-        document.write('<p><strong><em>' + week[i] + '</em></strong></p>');
-      } else {
-        document.write('<p><strong>' + week[i] + '</strong></p>');
-      };
-      break;
-    default:
-      if (week[i] == currentDay) {
-        document.write('<p><em>' + week[i] + '</em></p>');
-      } else {
-        document.write('<p>' + week[i] + '</p>');
-      };
-      break;
+str = str[0].toUpperCase() + str.slice(1);
+
+
+// Задание 2
+let pos = -1;
+while ((pos = str.indexOf('-', pos + 1)) != -1) {
+  str = str.substring(0, pos) + " " + str.slice(pos + 1);
+}
+
+console.log(str);
+
+// Задание 3
+let substr = 'легким';
+pos = str.indexOf(substr);
+if (~pos) {
+  substr = str.slice(0, -2) + "o";
+  str = str.substring(0, pos) + substr + str.slice(pos + substr.length + 1);
+  console.log(str);
+} else {
+  console.log("Слово " + substr + " не найдено!");
+}
+
+/* Задание 4 
+У вас также есть массив arr = [20, 33, 1, “Человек”, 2, 3]
+Вывести в консоль квадратный корень из суммы кубов его элементов(Да, человека нужно исключить) * /
+ */
+
+let arr = [20, 33, 1, "Человек", 2, 3]
+let sum = 0;
+arr.forEach(element => {
+  if (typeof(element) == "number") {
+    sum += Math.pow(element, 3);
   }
-};
+});
+console.log("Квадратный корень из суммы кубов: " + Math.sqrt(sum));
 
-let arr = ['34234', '748479', '90866789', '53656', '4256', '7245', '33333'];
-
-for (let i = 0; i < arr.length; i++) {
-  if ((arr[i].substr(0, 1) == 3) || (arr[i].substr(0, 1) == 7)) {
-    console.log(arr[i]);
+// Задание 5
+function toTrimmerStr(str) {
+  if (isNaN(+str) && (typeof(str) == "string") ) {
+    str = str.trim();
+    if (str.length > 50){
+      str = str.substr(0, 50) + '...';
+    }
+    alert(str);
+  } else {
+    alert ("Аргументом функции toTrimmerStr() не является строка!");
   }
 }
+str = prompt("Введите произвольную строку");
+toTrimmerStr(str);

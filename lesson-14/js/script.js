19fc6385573884d0ae45d4b5252913b3cd30a2e6
Tatiana <batagaeva.ta@gmail.com>
2019-03-22 11:41:44 +0800

    Добавлен урок 14

--- lesson-14/js/script.js
+++ lesson-14/js/script.js
@@ -3,25 +3,35 @@ $(document).ready(function () {
   $('a[href="#sheldure"]').on('click', open);
   $('.close').on('click', close);
   function open() {
-    $('.overlay').show().css('opacity', 0).animate({
-      opacity: 1
-    }, 1000);
-    $('.modal').show().css('margin-top', "-500px").animate({
-      "margin-top": "5rem"
-    }, 1000);
-  };
+    $('.overlay').fadeIn(1000);
+    $('.modal').slideDown(1000);
+  }
 
   
   function close() {
-    $.when($('.modal').css('margin-top', "5rem").animate({
-        "margin-top": "-500px"
-      }, 1000),
-      $('.overlay').css('opacity', 1).animate({
-        "opacity": "0"
-      }, 1000))
-      .then(function(){
-        $('.overlay').hide();
-        $('.modal').hide();
-      });
+    $('.overlay').fadeOut(1000);
+    $('.modal').slideUp(1000);
   };
+  // function open() {
+  //   $('.overlay').show().css('opacity', 0).animate({
+  //     opacity: 1
+  //   }, 1000);
+  //   $('.modal').show().css('margin-top', "-500px").animate({
+  //     "margin-top": "5rem"
+  //   }, 1000);
+  // };
+
+  
+  // function close() {
+  //   $.when($('.modal').css('margin-top', "5rem").animate({
+  //       "margin-top": "-500px"
+  //     }, 1000),
+  //     $('.overlay').css('opacity', 1).animate({
+  //       "opacity": "0"
+  //     }, 1000))
+  //     .then(function(){
+  //       $('.overlay').hide();
+  //       $('.modal').hide();
+  //     });
+  // };
 });
